4753a9a799790075170be5738bd8d268f3f84ce0
DEYYF <unaifago@gmail.com>
2024-05-08 14:00:28 +0200

    Manifest consumir Apis

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,8 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+
     // Retrofit
     implementation ("com.squareup.retrofit2:retrofit:$retrofit_version")
     // Gson
